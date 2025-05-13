9ec7b319129b15b21bcc689ceea273cd114c79a4
Копылов Владимир <ruwer555@mail.ru>
2025-05-13 23:43:12 +0300

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и философский камень',
+        publicationYear: 1997,
+        price: 899,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 450,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 350,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Антуан де Сент-Экзюпери',
+        name: 'Маленький принц',
+        publicationYear: 1943,
+        price: 299,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 1869,
+        price: 1200,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Эрих Мария Ремарк',
+        name: 'Три товарища',
+        publicationYear: 1936,
+        price: 550,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 499,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Эрнест Хемингуэй',
+        name: 'Старик и море',
+        publicationYear: 1952,
+        price: 399,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Дэн Браун',
+        name: 'Код да Винчи',
+        publicationYear: 2003,
+        price: 699,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джейн Остин',
+        name: 'Гордость и предубеждение',
+        publicationYear: 1813,
+        price: 425,
+        bestseller: false,
+        inStock: false
+    }
+];
