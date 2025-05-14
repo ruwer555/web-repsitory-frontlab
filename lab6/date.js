36411d50d93b8fb603d1fa0d343b0cbb0eade69c
Копылов Владимир <ruwer555@mail.ru>
2025-05-14 21:13:47 +0300

    Вывод даты

---
+++ lab6/date.js
@@ -0,0 +1,23 @@
+// data.js
+const data = [
+    {
+        Data: "Русская",
+        local: 'ru-RU'
+    },
+    {
+        Data: "Бельгийская",
+        local: 'fr-BE'
+    },
+    {
+        Data: "Испанская",
+        local: 'es-CR'
+    },
+    {
+        Data: "Голландская",
+        local: 'nl-NL'
+    },
+    {
+        Data: "Албанская",
+        local: 'sq-AL'
+    }
+];
